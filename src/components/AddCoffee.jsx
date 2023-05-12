db85fcbfffeb987e44060799f1e2f3eb01d419d1
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-12 12:32:36 +0600

    added sweet alert

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import Swal from "sweetalert2";
 
 const AddCoffee = () => {
   const handelAddCoffee = (event) => {
     event.preventDefault();
+
+    const form = event.target;
+    const name = form.name.value;
+    const quantity = form.quantity.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const details = form.details.value;
+    const photo = form.photo.value;
+
+    const newCoffee = {
+      name,
+      quantity,
+      supplier,
+      taste,
+      category,
+      details,
+      photo,
+    };
+    console.log(newCoffee);
+
+    //send data to the server
+    fetch("http://localhost:5000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
   };
 
   return (
@@ -47,7 +81,7 @@ const AddCoffee = () => {
             <label className="input-group">
               <input
                 type="text"
-                name="name"
+                name="supplier"
                 placeholder="Supplier"
                 className="input input-bordered w-full"
               />
